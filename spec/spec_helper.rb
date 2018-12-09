872aafc2762d3c2cf3a7a106f7dd350c2330cb44
Nitesh Varma <varmanitesh91@gmail.com>
2018-12-09 22:19:28 +0530

    Added Benchmark Specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require "bundler/setup"
 require "phone_number_mapper"
+require "rspec-benchmark"
 
 RSpec.configure do |config|
+  config.include RSpec::Benchmark::Matchers
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
